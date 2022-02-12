e9a9c1bfe8dc45b27feac9eafb8b3a58072aba5c
rainett <86346775+valera3450zhma@users.noreply.github.com>
2022-02-12 14:39:09 +0200

    firstCommit

---
+++ main.py
@@ -0,0 +1,36 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+
+from time import sleep
+
+app = Client("my_account", api_id=876100, api_hash="ab03c3758ababdad2d8859e08244ae40")
+
+
+@app.on_message(filters.command("print", "!") & filters.me)
+def type_style(_, message):
+    text = message.text.split("!print ", maxsplit=1)[1]
+    to_print = ""
+    typing_symbol = '▒'
+    while len(text) != 0:
+        try:
+            message.edit(to_print + typing_symbol)
+            sleep(0.002)
+            to_print += text[0]
+            text = text[1:]
+            message.edit(to_print)
+            sleep(0.001)
+        except FloodWait as e:
+            sleep(e.x)
+
+
+@app.on_message(filters.command("rusak", "!") & filters.me)
+def rusak_battle(_, message):
+    times = int(message.text[6:])
+    print(times)
+    for i in range(times):
+        bot_results = app.get_inline_bot_results("Random_UAbot")
+        app.send_inline_bot_result(f"{message.chat.id}", bot_results.query_id, bot_results.results[0].id)
+        sleep(0.1)
+
+
+app.run()
