8dec05f7c0a43e723938c28c5e0d67185d51a142
rainett <86346775+valera3450zhma@users.noreply.github.com>
2022-02-19 14:46:24 +0200

    updated

--- main.py
+++ main.py
@@ -30,16 +30,31 @@ def rusak_battle(_, message):
     app.delete_messages(chat_id, message.message_id)
     for i in range(times):
         try:
-            if i % 30 == 1:
-                sent = app.send_message("Random_UAbot", "/shop")
-                bot_message = app.get_messages("Random_UAbot", sent.message_id+1)
-                bot_message.click(3)
-
+            if i % 10 == 0:
+                buy_heal(_, message)
             bot_results = app.get_inline_bot_results("Random_UAbot")
             app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[0].id)
             sleep(2)
         except FloodWait as e:
             sleep(e.x)
+
+
+@app.on_message(filters.command("heal", "!"))
+def buy_heal(_, message):
+    chat_id = message.chat.id
+    app.send_message(chat_id, "чекаю хп в боті")
+    sent = app.send_message("Random_UAbot", "/rusak")
+    sleep(2)
+    health = int(app.get_messages("Random_UAbot", sent.message_id + 1).caption.split()[18])
+    if health < 30:
+        app.send_message(chat_id, "треба аптечку")
+        sent = app.send_message("Random_UAbot", "/shop")
+        sleep(2)
+        bot_shop = app.get_messages("Random_UAbot", sent.message_id + 1)
+        bot_shop.click(3)
+        app.send_message(chat_id, "купив аптечку")
+    else:
+        app.send_message(chat_id, "норм, жити можна")
 
 
 app.run()
